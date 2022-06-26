afdc35ed013daaff71ddf32c14a1566feb01ab61
yunusmohammedjemal <yunusmohammed767@gmail.com>
2022-06-26 09:47:49 -0600

    create guess game

---
+++ Guess_game/my_project/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace nm{
+    class my_p
+    {
+        static void Main(String[] args)
+        {
+            int i;
+            Random rand = new Random();
+            int secret_no = rand.Next(100);
+            Console.WriteLine("Guess the no from 0 to 100");
+            for ( i = 0; i < 10; i++)
+            {
+                string gue = Console.ReadLine();
+                int guess = Convert.ToInt32(gue);
+                
+                if (guess == secret_no)
+                {
+                    Console.WriteLine("corrrect guess,you won!!!");
+                    break;
+
+                }
+                else if (guess > secret_no)
+                    Console.WriteLine("the number is less than " + guess);
+                else if (guess < secret_no)
+                    Console.WriteLine("the number is greater than " + guess);
+                else
+                    Console.WriteLine("try again you have " + (10 - i) + " chances left");
+            }
+            if (i == 10)
+                Console.WriteLine("Game over,you have finished your chances");
+        }
+    } 
+}
